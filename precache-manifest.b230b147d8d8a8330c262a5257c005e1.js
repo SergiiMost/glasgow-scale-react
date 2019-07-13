0eab1538f66d01576842450d5096e412b5efd7cb
SergiiMost <s.v.mostovoyi@gmail.com>
2019-07-12 22:22:53 -0400

    Updates

---
+++ precache-manifest.b230b147d8d8a8330c262a5257c005e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efda21131e768ba33ea53f97f1221c44",
+    "url": "/glasgow-scale-react/index.html"
+  },
+  {
+    "revision": "42f0c4d7b7e0126f8f09",
+    "url": "/glasgow-scale-react/static/css/main.ea536655.chunk.css"
+  },
+  {
+    "revision": "c8ed54fc815c84e03a7c",
+    "url": "/glasgow-scale-react/static/js/2.b33f980e.chunk.js"
+  },
+  {
+    "revision": "42f0c4d7b7e0126f8f09",
+    "url": "/glasgow-scale-react/static/js/main.a428d4ce.chunk.js"
+  },
+  {
+    "revision": "d48a265c2a517467d5a9",
+    "url": "/glasgow-scale-react/static/js/runtime~main.6e573478.js"
+  }
+]);
